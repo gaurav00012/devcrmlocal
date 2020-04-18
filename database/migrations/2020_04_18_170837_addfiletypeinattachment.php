eb94c3e6f7e6a9e36c518877f7619daec8c1183a
Gaurav <aggarwal.gaurav611@gmail.com>
2020-04-19 00:11:56 +0530

    changes in table

---
+++ database/migrations/2020_04_18_170837_addfiletypeinattachment.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Addfiletypeinattachment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("
+        ALTER TABLE `devcrm`.`mas_task_attachments`   
+        ADD COLUMN `file_type` VARCHAR(25) NULL AFTER `file_name`;
+      
+
+          ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
