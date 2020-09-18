873a84b570af76e7186b7483bbbfce3c02b87a62
gaurav00012 <aggarwal.gaurav611@gmail.com>
2020-09-18 06:44:47 +0530

    changes in view

---
+++ resources/views/frontend/web/registermail.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<p>Company Name: {{$form['company_name']}}</p>
+	<p>Contact Person: {{$form['contact_person']}}</p>
+	<p>Email address : {{$form['email']}}</p>
+	<p>Mailing address: {{$form['mailing_address']}}</p>
+	<p>Tell Us about your business: {{$form['about_business']}}</p>
+	<p>Do you have domain name? (If not, does one need to be purchased for you ?): {{$form['domain_name']}}</p>
+	<p>Do you have access to your hosting? (Cpanel/FTP & pHpMyAdmin/ GoDaddy) What are credential? : {{$form['hosting_access']}}</p>
+	<p>Where are you hosting your emails?: {{$form['hosting_email']}}</p>
+	<p>Image & video content (Please upload all the content to a google drive/dropbox folder and share it with eldermonks@gmail.com or enter a gmail address below, we will share the folder with you to upload the files): {{$form['content']}}</p>
+	<p>Elevator Pitch/ Sales Pitch: {{$form['copy']}}</p>
+	<p>Define primary goal (Sell Online, Traffic, Contact Form, Lead Generation, Marketing, Informational): {{$form['primary_goal']}}</p>
+	<p>Target geographic (location): {{$form['target_geographic']}}</p>
+	<p>Target audience (demographics): {{$form['target_audience']}}</p>
+	<p>Pick the word that best describes the message you want to convey: {{$form['describe_word_1']}}</p>
+	<p>Pick the word that best describes the feeling you want to convey: {{$form['describe_word_2']}}</p>
+	<p>Pick the word that best describes the look you want to convey: {{$form['describe_word_3']}}</p>
+	<p>Company Colors: {{$form['company_colors']}}</p>
+	<p>Navigation(Home / About Us / Contact Us .... ): {{$form['navigation']}}</p>
+	<p>Competitors: {{$form['competitors']}}</p>
+	<p>Reference websites: {{$form['reference']}}</p>
+	<p>Additional notes: {{$form['additional_notes']}}</p>
+
+
+</body>
+</html>
